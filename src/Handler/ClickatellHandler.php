aef6cd3dfe086ebe7d4460cf590dce44143e9d83
Itay Elkouby <itaye@example.com>
2017-04-30 14:23:47 +0300

    Remove the sid, as clickatell doesn't use it

--- src/Handler/ClickatellHandler.php
+++ src/Handler/ClickatellHandler.php
@@ -17,23 +17,22 @@ class ClickatellHandler extends SMSHandler
     const API_V1 = '2010-04-01';
 
     /**
-     * @param string $secret     Twilio API Secret Token
-     * @param string $sid        Twilio API SID
+     * @param string $secret     Twilio API Secret Token     
      * @param string $fromNumber The phone number that will be shown as the sender ID
      * @param string $toNumber   The phone number to which the message will be sent
      * @param int    $level      The minimum logging level at which this handler will be triggered
      * @param bool   $bubble     Whether the messages that are handled can bubble up the stack or not
      * @param bool   $useSSL     Whether to connect via SSL.
      * @param string $host       The Twilio server hostname.
-     * @param string $version    The Twilio API version (default TwilioHandler::API_V1)
+     * @param string $version    The Twilio API version (default ClickatellHandler::API_V1)
      * @param string $limit      The character limit
      */
-    public function __construct($secret, $sid, $fromNumber, $toNumber, $level = Logger::CRITICAL, $bubble = true, $useSSL = true, $host = 'platform.clickatell.com', $version = self::API_V1, $limit = 160)
+    public function __construct($secret, $fromNumber, $toNumber, $level = Logger::CRITICAL, $bubble = true, $useSSL = true, $host = 'platform.clickatell.com', $version = self::API_V1, $limit = 160)
     {
         if($version !== self::API_V1){
             throw new Exception('API Version \'{$version}\' is not supported!');
         }
-        parent::__construct($secret, $sid, $fromNumber, $toNumber, $level, $bubble, $useSSL, $host, $version, $limit);
+        parent::__construct($secret, null, $fromNumber, $toNumber, $level, $bubble, $useSSL, $host, $version, $limit);
     }
 
     /**
